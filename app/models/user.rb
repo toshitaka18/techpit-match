013b62f5cc37de96a2f2a50b0b05ff133d3444a1
toshitaka18 <toshitaka0018@gmail.com>
2020-02-04 03:03:33 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :chat_messages
   has_many :chat_room_users
+  has_one_attached :image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
